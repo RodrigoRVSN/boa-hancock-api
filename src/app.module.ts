e38a5daf724b53a790eea2eb6968031506577298
RodrigoRVSN <rodrigovictor81@gmail.com>
2022-11-15 12:11:16 -0300

    chore: move localhost variables to .env before deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { factoriesModules } from './factories';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import * as redisStore from 'cache-manager-redis-store';
 
+const configService = new ConfigService();
 @Module({
   imports: [
     AuthModule,
@@ -15,7 +16,7 @@ import * as redisStore from 'cache-manager-redis-store';
       // @ts-ignore
       store: redisStore,
       socket: {
-        host: 'localhost',
+        host: configService.get('REDIS_URL'),
         port: 6379,
       },
     }),
