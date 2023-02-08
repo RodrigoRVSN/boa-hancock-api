6cc981cb331f3d0bd24f1cd80a73fa7c1ec033ce
rodrigorvsn <rodrigovictor81@gmail.com>
2023-02-08 18:37:57 -0300

    Revert "chore: test"
    This reverts commit 457e580841b9362ccf1ee6e08b008e86eeac2967.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { factoriesModules } from './factories';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import * as redisStore from 'cache-manager-redis-store';
 
 const configService = new ConfigService();
 @Module({
@@ -11,9 +12,14 @@ const configService = new ConfigService();
     CacheModule.register({
       isGlobal: true,
       // @ts-ignore
+      store: redisStore,
+      socket: {
+        host: configService.get('REDIS_URL'),
+        port: 6379,
+      },
     }),
   ],
   controllers: [],
   providers: [{ provide: APP_INTERCEPTOR, useClass: CacheInterceptor }],
 })
-export class AppModule { }
+export class AppModule {}
